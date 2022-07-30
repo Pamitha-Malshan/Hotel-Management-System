08a72b7542bde1ab786e12f32a22994d83b6b92f
IT20127800 <IT20127800@my.sliit.lk>
2022-07-30 07:34:02 +0530

    Setup Application

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const multer = require('multer');
+
+
+
+const app = express();
+
+
+
+//app middleware
+app.use(bodyParser.json());
+app.use(cors());
+//app.use(multer());
+
+
+
+
+const PORT = 8001;
+const DB_URL = 'mongodb+srv://pamitha:pamitha@database1.gqpga.mongodb.net/HOTELSYSTEM?retryWrites=true&w=majority';
+
+mongoose.connect(DB_URL)
+.then(() =>{
+    console.log('DB connected');
+}).catch((err) => console.log('DB connection error', err));
+
+app.listen(process.env.PORT || PORT, () =>{
+    console.log(`App is running on ${PORT}`);
+});
+
